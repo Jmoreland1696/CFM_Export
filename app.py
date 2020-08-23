9e6202ce94148936e856d1821ae55d24b434928c
Jmoreland1696 <58407980+Jmoreland1696@users.noreply.github.com>
2020-08-22 22:47:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,21 @@ from flask import Flask, request
 firebase_key = os.environ['firebase_credentials'].replace("\\n","\n")
 databaseURL = os.environ['firebase_url']
 
+ENV_KEYS = {
+	"type": "service_account",
+	"project_id": os.environ["project_id"]
+	"private_key_id": os.environ["private_key_id"],
+	"private_key": os.environ["private_key"].replace("\\n", "\n"),
+	"client_email": os.environ["client_email"],
+	"client_id": os.environ["client_id"],
+	"auth_uri": os.environ["auth_uri"],
+	"token_uri": os.environ["token_uri"],
+	"auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
+	"client_x509_cert_url": os.environ["client_x509_cert_url"]
+	}
+
 # Connecting
-cred = credentials.Certificate(firebase_key)
+cred = credentials.Certificate(ENV_KEYS)
 firebase_admin.initialize_app(cred, {'databaseURL': databaseURL})
 
 # Initialize App
